33b21715b2aff36df1b1ba8c569dfeb539eff41e
Amir Daniali <amir@amirdaniali.com>
2025-11-05 10:59:37 -0500

    Setup Shadcn UI with night toggle with react router

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,22 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import { Button } from "~/components/ui/button"
+
+import type { Route } from "./+types/home"
+import {ScrollRestoration} from "react-router";
+import {ThemeProvider} from "~/components/theme-provider";
+import {ModeToggle} from "~/components/mode-toggle";
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+    return [
+        { title: "New React Router App" },
+        { name: "description", content: "Welcome to React Router!" },
+    ]
 }
 
 export default function Home() {
-  return <Welcome />;
+    return (
+        <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+            <Button>Click me</Button>
+            <ModeToggle></ModeToggle>
+        </ThemeProvider>
+    )
 }
